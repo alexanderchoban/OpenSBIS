942cb048bbd83838334a0948776688e35d931172
Alexander Choban <abchoban@whateverittakes.com>
2017-07-24 16:29:39 -0500

    Removed sample fetch data component.

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { CompaniesList } from './components/Companies/CompaniesList';
 import { AddCompany } from './components/Companies/AddCompany';
@@ -10,7 +9,6 @@ import { AddCompany } from './components/Companies/AddCompany';
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
     <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
     <Route path='/companies' component={ CompaniesList } />
     <Route path='/newcompany' component={ AddCompany } />
 </Layout>;
